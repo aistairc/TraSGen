3a600864c6f8cf40d981256341f59cf30119320f
Komal Mariam <komal.mariam@gmail.com>
2023-11-22 17:16:18 +0900

    Removed non-network files, added license

--- src/main/java/DataGen/StreamingJob.java
+++ src/main/java/DataGen/StreamingJob.java
@@ -43,18 +43,6 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 
-/**
- * Skeleton for a Flink Streaming Job.
- *
- * <p>For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
 public class StreamingJob implements Serializable {
 
 	public static void main(String[] args) throws Exception {
